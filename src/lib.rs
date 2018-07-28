653b6f1973ff2519fc54c8ed39e2ced084d7e13e
Richard Stewing <richy.sting@gmail.com>
2018-07-28 18:12:31 +0200

    push up and down

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ mod tests {
             length: 4,
         };
         row_1.push_right();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
     }
 
     #[test]
@@ -51,7 +51,7 @@ mod tests {
             length: 4,
         };
         row_1.swipe_right();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
         let mut row_1 = Row {
             row: vec![Some(2), Some(2), Some(2), Some(2)],
@@ -62,7 +62,7 @@ mod tests {
             length: 4,
         };
         row_1.swipe_right();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
         let mut row_1 = Row {
             row: vec![Some(2), Some(2), Some(2), Some(1)],
@@ -73,7 +73,7 @@ mod tests {
             length: 4,
         };
         row_1.swipe_right();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
     }
 
@@ -91,11 +91,11 @@ mod tests {
             length: 4,
         };
         row_1.push_left();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
     }
 
 
-        #[test]
+    #[test]
     fn swipe_left() {
         use data::Row;
         
@@ -108,7 +108,7 @@ mod tests {
             length: 4,
         };
         row_1.swipe_left();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
         let mut row_1 = Row {
             row: vec![Some(2), Some(2), Some(2), Some(2)],
@@ -119,7 +119,7 @@ mod tests {
             length: 4,
         };
         row_1.swipe_left();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
         let mut row_1 = Row {
             row: vec![Some(1), Some(2), Some(2), Some(2)],
@@ -130,19 +130,229 @@ mod tests {
             length: 4,
         };
         row_1.swipe_left();
-        assert_eq!(row_1.row, row_2.row);
+        assert_eq!(row_1, row_2);
 
     }
+
+    #[test]
+    fn push_up() {
+        use data::Row;
+        use data::Field;
+
+        let mut row_1_1 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_1_2 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_1_3 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_1_4 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_2_1 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_2_2 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_2_3 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_2_4 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+    
+
+        let mut field_1 = Field {
+            rows: vec![row_1_1,
+                       row_1_2,
+                       row_1_3,
+                       row_1_4],
+            size: 4,
+        };
+        let mut field_2 = Field {
+            rows: vec![row_2_1,
+                       row_2_2,
+                       row_2_3,
+                       row_2_4],
+            size: 4,
+        };
+
+        field_1.push_up();
+        assert_eq!(field_1, field_2);
+    }        
+
+    #[test]
+    fn push_down() {
+        use data::Row;
+        use data::Field;
+
+        let mut row_1_1 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_1_2 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_1_3 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_1_4 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_2_1 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_2_2 = Row {
+            row: vec![Some(2), Some(2), Some(2), Some(2)],
+            length: 4,
+        };
+
+        let mut row_2_3 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+
+        let mut row_2_4 = Row {
+            row: vec![None, None, None, None],
+            length: 4,
+        };
+    
+
+        let mut field_1 = Field {
+            rows: vec![row_1_1,
+                       row_1_2,
+                       row_1_3,
+                       row_1_4],
+            size: 4,
+        };
+        let mut field_2 = Field {
+            rows: vec![row_2_3,
+                       row_2_4,
+                       row_2_1,
+                       row_2_2],
+            size: 4,
+        };
+
+        field_1.push_down();
+        assert_eq!(field_1, field_2);
+    }        
+
+
+    
 
 }
 
 mod data {
     use std::mem::swap;
 
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     pub struct Row {
         pub row: Vec<Option<usize>>,
         pub length: usize,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct Field {
+        pub rows: Vec<Row>,
+        pub size: usize,
+    }
+
+    impl Field {
+        pub fn new(size: usize) -> Field {
+            let mut field = Vec::with_capacity(size);
+            for _ in 0 .. size {
+                field.push(Row::new(size));
+            }
+            Field {
+                rows: field,
+                size: size,
+            }
+
+        }
+
+        pub fn swipe_left(&mut self) {
+            for row in &mut self.rows {
+                row.swipe_left();
+            }
+            
+        }
+
+        pub fn swipe_right(&mut self) {
+            for row in &mut self.rows {
+                row.swipe_right();
+            }
+            
+        }
+
+        pub fn push_up_single(&mut self, i: usize) {
+            let mut collected = Vec::with_capacity(self.size);
+            for j in 0 .. self.size {
+                let mut tmp = None;
+                swap(&mut tmp, &mut self.rows[j].row[i]);
+                if tmp != None {
+                    collected.push(tmp);
+                } 
+            }
+            for j in 0 .. collected.len() {
+                self.rows[j].row[i] = collected[j];
+            }
+        }
+
+        pub fn push_up(&mut self){
+            for i in 0..self.size {
+                self.push_up_single(i);
+            }
+        }
+
+        pub fn push_down_single(&mut self, i: usize) {
+            let mut collected = Vec::with_capacity(self.size);
+            for j in 0 .. self.size {
+                let index = self.size - 1 - j;
+                let mut tmp = None;
+                swap(&mut tmp, &mut self.rows[index].row[i]);
+                if tmp != None {
+                    collected.push(tmp);
+                } 
+            }
+            for j in 0 .. collected.len() {
+                let index = self.size - 1 - j;
+                self.rows[index].row[i] = collected[j];
+            }
+        }
+
+        pub fn push_down(&mut self){
+            for i in 0..self.size {
+                self.push_down_single(i);
+            }
+        }
+
     }
 
     impl Row {
